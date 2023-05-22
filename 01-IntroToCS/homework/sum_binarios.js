d6e4878dcfd86438dbc53a5b90506e7653ad2cac
JalfMVZ <velascojosealfredo1@gmail.com>
2023-05-22 14:09:17 -0500

    prueba resuelta

---
+++ 01-IntroToCS/homework/sum_binarios.js
@@ -0,0 +1,23 @@
+/*
+Not ! !true -> === false
+Not ! !false -> === true
+*/
+
+/*
+A   B   AND     <-- carry
+1   1   1
+1   0   0
+0   0   0
+0   1   0
+*/
+
+/*
+A   B   XDR     <-- Suma
+1   1   0
+1   0   1
+0   0   0
+0   1   1
+*/  
+
+
+
