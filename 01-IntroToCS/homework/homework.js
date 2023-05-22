cda43e2875079aac02acb01bc7d1d8b34c376c51
JalfMVZ <velascojosealfredo1@gmail.com>
2023-05-22 16:51:34 -0500

    texto comentar

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,12 +3,21 @@
 function BinarioADecimal(num) {
    return parseInt(num, 2);
 }
+/* La función parseInt() se utiliza para analizar una cadena de texto 
+y convertirla en un número entero luego se le pasa el argumento num, 
+que es la cadena de texto que representa un número en formato binario
+*/
+
 
 function DecimalABinario(num) {
    return num.toString(2);
 }
 
+/*La instrucción return devuelve el resultado de la expresión num.toString(2) 
+y la función toString() se utiliza para convertir un número en una representación de cadena de texto */
+
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
-};
+}; 
+ 
